57ad8f2aa032c7dac4bf4f83c3e30ac2b91012a9
RakeshC <rakeshchandra9620@gmail.com>
2026-06-21 22:34:25 +0530

    integerated with testdata fille  with json format to testfiles

--- playwright.config.js
+++ playwright.config.js
@@ -14,8 +14,9 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  timeout :80000,
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,6 +1,30 @@
-import test from "@playwright/test";
+import { test, expect } from '@playwright/test';
+import data from "../Testdata/logindata.json"
+test('verify valid cred', async ({ page }) => {
+  await page.goto('https://petrouat.ymtsindia.in/');
+  await page.getByRole('textbox', { name: 'Welcome Again' }).click();
+  await page.getByRole('textbox', { name: 'Welcome Again' }).click();
+  await page.getByRole('textbox', { name: 'Welcome Again' }).fill(data.Reference_code);
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await page.getByText('Username').click();
+  await page.getByRole('textbox', { name: 'Username' }).fill(data.username);
+  await page.getByRole('textbox', { name: 'Username' }).press('Enter');
+  await page.getByText('Password', { exact: true }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill(data.password);
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('link', { name: 'SIVA AGENCIES' })).toBeVisible();
+  await page.close
+});
 
-test('verify valid cred', async({page}) => {
-await page.goto("https://github.com/rakeshc1813-netizen/Rakeshlearning-playwright")
-
+test('verify with invalid password', async ({ page }) => {
+  await page.goto('https://petrouat.ymtsindia.in/');
+  await page.getByRole('textbox', { name: 'Welcome Again' }).click();
+  await page.getByRole('textbox', { name: 'Welcome Again' }).fill('BZ2010126');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('admin');
+  await page.getByText('Password', { exact: true }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin12');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByText('Invalid Login Credentials')).toBeVisible();
 });
